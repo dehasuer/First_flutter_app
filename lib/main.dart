a9299cd63ff2b7ad2ab9e2eb62feeb83770a41bf
Deha Efecan Süer <dehasuer97@gmail.com>
2023-02-19 02:01:32 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.teal, accentColor: Colors.amber),
       home: Scaffold(
+        backgroundColor: Colors.yellow,
         appBar: AppBar(
           title: Text("Başlık"),
           backgroundColor: Colors.deepPurple,
         ),
-        body: Text(
-          "Hello World!",
-          style: TextStyle(color: Colors.amber, fontSize: 25),
+        body: Container(
+          alignment: Alignment.topCenter,
+          width: 200,
+          height: 300,
+          color: Colors.purple,
+          child: Text(
+            "Deha" * 1,
+            textAlign: TextAlign.center,
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
