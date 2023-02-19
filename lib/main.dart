15808d6e0c56db0085e351deff806ae09d1806e8
Deha Efecan Süer <dehasuer97@gmail.com>
2023-02-19 22:18:42 +0300

    edited

--- lib/main.dart
+++ lib/main.dart
@@ -5,28 +5,48 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  String _img1 =
+      "https://www.bifikir.com/images/content/lamborghini-huracan-1_g3er.webp";
+  String _img2 =
+      "https://emrealtunbilek.com/wp-content/uploads/2016/10/apple-icon-72x72.png";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.teal, accentColor: Colors.purple),
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Başlık"),
+          title: Text("Title"),
           backgroundColor: Colors.deepPurple,
         ),
         body: Center(
           child: Container(
-            width: 200,
-            height: 200,
-            color: Colors.red,
-            child: Container(
-              alignment: Alignment.center,
-              color: Colors.blue,
-              margin: EdgeInsets.all(30),
-              width: 50,
-              height: 50,
-              child: Text("Deha"),
+            padding: EdgeInsets.all(20),
+            child: Text(
+              "Deha",
+              style: TextStyle(fontSize: 36),
             ),
+            decoration: BoxDecoration(
+                color: Colors.orange,
+                shape: BoxShape.rectangle,
+                border: Border.all(width: 4, color: Colors.purple),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+                image: DecorationImage(
+                    image: NetworkImage(_img2),
+                    fit: BoxFit.scaleDown,
+                    repeat: ImageRepeat.repeat),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.green,
+                      offset: Offset(0, 20),
+                      blurRadius: 20),
+                  BoxShadow(
+                      color: Colors.yellow,
+                      offset: Offset(0, -20),
+                      blurRadius: 10),
+                ]),
           ),
         ),
         floatingActionButton: FloatingActionButton(
